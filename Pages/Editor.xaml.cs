16ccf602a4e8686da27dde64f2ccf2fb2fc1a39d
ornaras <ornaras.us@gmail.com>
2025-04-25 10:28:41 +0400

    Добавление редактора

--- Pages/Editor.xaml.cs
+++ Pages/Editor.xaml.cs
@@ -26,7 +26,6 @@ namespace RetailCorrector.Wizard.Pages
         DateTime,
         TotalSum
     }
-
     public class FilterItem : INotifyPropertyChanged
     {
         public bool IsEnabled
@@ -36,7 +35,7 @@ namespace RetailCorrector.Wizard.Pages
             {
                 if (isEnabled == value) return;
                 isEnabled = value;
-                Editor.RefreshPreview();
+                Editor.RefreshFilterPreview();
                 OnPropertyChanged();
             }
         }
@@ -47,7 +46,7 @@ namespace RetailCorrector.Wizard.Pages
             {
                 if (property == value) return;
                 property = value;
-                Editor.RefreshPreview();
+                Editor.RefreshFilterPreview();
                 OnPropertyChanged();
             }
         }
@@ -58,7 +57,7 @@ namespace RetailCorrector.Wizard.Pages
             {
                 if (pattern == value) return;
                 pattern = value;
-                Editor.RefreshPreview();
+                Editor.RefreshFilterPreview();
                 OnPropertyChanged();
             }
         }
@@ -69,7 +68,120 @@ namespace RetailCorrector.Wizard.Pages
         private string pattern = "";
         private void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 
+    public enum PropertyType
+    {
+        Receipt,
+        Position
+    }
+    public enum ReceiptProperty
+    {
+        PaymentCash,
+        PaymentEcash,
+        PaymentPre,
+        PaymentPost,
+        PaymentProvision,
+        Operation,
+        //DateTime,
+        TotalSum
+    }
+    public enum PositionProperty
+    {
+        Name,
+        Quantity,
+        Price,
+        Sum,
+        Measure,
+        Payment,
+        Type,
+        Tax
+    }
+    public class EditorItem : INotifyPropertyChanged
+    {
+        public PropertyType DataType
+        {
+            get => _dataType;
+            set
+            {
+                if (_dataType == value) return;
+                _dataType = value;
+                Editor.RefreshEditorPreview();
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(EnumValues));
+                OnPropertyChanged(nameof(ConditionPropertyName));
+                OnPropertyChanged(nameof(PropertyName));
+            }
+        }
+        public Array EnumValues => 
+            Enum.GetValues(
+                _dataType == PropertyType.Receipt ? 
+                typeof(ReceiptProperty) : 
+                typeof(PositionProperty));
+        public int ConditionPropertyId
+        {
+            get => _conditionId;
+            set
+            {
+                if (_conditionId == value) return;
+                _conditionId = value;
+                Editor.RefreshEditorPreview();
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ConditionPropertyName));
+            }
+        }
+        public string ConditionPropertyName =>
+            _dataType == PropertyType.Receipt ?
+            ((ReceiptProperty)_conditionId).ToString() :
+            ((PositionProperty)_conditionId).ToString();
+        public string ConditionPropertyPattern
+        {
+            get => _pattern;
+            set
+            {
+                if (_pattern == value) return;
+                _pattern = value;
+                Editor.RefreshEditorPreview();
+                OnPropertyChanged();
+            }
+        }
+        public int PropertyId
+        {
+            get => _propId;
+            set
+            {
+                if (_propId == value) return;
+                _propId = value;
+                Editor.RefreshEditorPreview();
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PropertyName));
+            }
+        }
+        public string PropertyName =>
+            _dataType == PropertyType.Receipt ?
+            ((ReceiptProperty)_propId).ToString() :
+            ((PositionProperty)_propId).ToString();
+        public string PropertyValue
+        {
+            get => _propValue;
+            set
+            {
+                if (_propValue == value) return;
+                _propValue = value;
+                Editor.RefreshEditorPreview();
+                OnPropertyChanged();
+            }
+        }
+
+        private PropertyType _dataType = PropertyType.Receipt;
+        private int _conditionId = 0;
+        private string _pattern = "";
+        private int _propId = 0;
+        private string _propValue = "";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
     /// <summary>
@@ -77,17 +189,121 @@ namespace RetailCorrector.Wizard.Pages
     /// </summary>
     public partial class Editor : UserControl
     {
-        public ObservableCollection<FilterItem> Items { get; }
-        internal static ObservableCollection<FilterItem> StaticFilters => Instance.Items;
+        public ObservableCollection<FilterItem> FilterItems { get; }
+        public ObservableCollection<EditorItem> EditorItems { get; }
+        internal static ObservableCollection<FilterItem> StaticFilters => Instance.FilterItems;
+        internal static EditorItem[] StaticREditors => [..Instance.EditorItems.Where(e => e.DataType == PropertyType.Receipt)];
+        internal static EditorItem[] StaticPEditors => [.. Instance.EditorItems.Where(e => e.DataType == PropertyType.Position)];
         private static Editor Instance;
 
-        public static void RefreshPreview()
+        public static void RefreshFilterPreview()
         {
             App.Receipts.Filtered.Clear();
             App.Receipts.Edited.Clear();
             foreach (var rec in App.Receipts.Parsed)
                 App.Receipts.Filtered.Add(new Tuple<bool, Receipt>(Check(rec), rec));
         }
+
+        public static void RefreshEditorPreview()
+        {
+            App.Receipts.Edited.Clear();
+            foreach (var rec in App.Receipts.Filtered.Where(r => r.Item1).Select(r => r.Item2))
+                App.Receipts.Edited.Add(Edit(rec));
+        }
+
+        private static Receipt Edit(Receipt origin)
+        {
+            var res = origin.Clone();
+            foreach(var e in StaticREditors)
+            {
+                if (string.IsNullOrWhiteSpace(e.PropertyValue)) continue;
+                if (!CheckReceipt(origin, (ReceiptProperty)e.ConditionPropertyId, e.ConditionPropertyPattern)) continue;
+                switch ((ReceiptProperty)e.PropertyId)
+                {
+                    case ReceiptProperty.PaymentCash:
+                        var pay = res.Payment;
+                        pay.Cash = uint.Parse(e.PropertyValue);
+                        res.Payment = pay;
+                        break;
+                    case ReceiptProperty.PaymentEcash:
+                        pay = res.Payment;
+                        pay.ECash = uint.Parse(e.PropertyValue);
+                        res.Payment = pay;
+                        break;
+                    case ReceiptProperty.PaymentPre:
+                        pay = res.Payment;
+                        pay.Pre = uint.Parse(e.PropertyValue);
+                        res.Payment = pay;
+                        break;
+                    case ReceiptProperty.PaymentPost:
+                        pay = res.Payment;
+                        pay.Post = uint.Parse(e.PropertyValue);
+                        res.Payment = pay;
+                        break;
+                    case ReceiptProperty.PaymentProvision:
+                        pay = res.Payment;
+                        pay.Provision = uint.Parse(e.PropertyValue);
+                        res.Payment = pay;
+                        break;
+                    case ReceiptProperty.Operation: res.Operation = (Operation)int.Parse(e.PropertyValue); break;
+                    case ReceiptProperty.TotalSum: res.RoundedSum = uint.Parse(e.PropertyValue); break;
+                    default: break;
+                }
+            }
+            for(var i = 0; i < res.Items.Length; i++)
+            {
+                var pos = res.Items[i].Clone();
+                foreach(var e in StaticPEditors)
+                {
+                    if (string.IsNullOrWhiteSpace(e.PropertyValue)) continue;
+                    if (!CheckPosition(res.Items[i], (PositionProperty)e.ConditionPropertyId, e.ConditionPropertyPattern)) continue;
+                    switch ((PositionProperty)e.PropertyId)
+                    {
+                        case PositionProperty.Name: pos.Name = e.PropertyValue; break;
+                        case PositionProperty.Measure: pos.MeasureUnit = (MeasureUnit)uint.Parse(e.PropertyValue); break;
+                        case PositionProperty.Payment: pos.PayType = (PaymentType)uint.Parse(e.PropertyValue); break;
+                        case PositionProperty.Price: pos.Price = uint.Parse(e.PropertyValue); break;
+                        case PositionProperty.Quantity: 
+                            pos.Quantity = uint.Parse(e.PropertyValue);
+                            pos.TotalSum = (uint)(pos.Quantity * pos.Price / 1000.0);
+                            break;
+                        case PositionProperty.Sum: pos.TotalSum = uint.Parse(e.PropertyValue); break;
+                        case PositionProperty.Tax: pos.TaxRate = (TaxRate)uint.Parse(e.PropertyValue); break;
+                        case PositionProperty.Type: pos.PosType = (PositionType)uint.Parse(e.PropertyValue); break;
+                        default: break;
+                    }
+                }
+                res.Items[i] = pos;
+            }
+            return res;
+        }
+
+        private static bool CheckReceipt(Receipt receipt, ReceiptProperty type, string pattern) =>
+            type switch
+            {
+                ReceiptProperty.PaymentPre => $"{receipt.Payment.Pre}" == pattern,
+                ReceiptProperty.PaymentPost => $"{receipt.Payment.Post}" == pattern,
+                ReceiptProperty.PaymentCash => $"{receipt.Payment.Cash}" == pattern,
+                ReceiptProperty.PaymentEcash => $"{receipt.Payment.ECash}" == pattern,
+                ReceiptProperty.PaymentProvision => $"{receipt.Payment.Provision}" == pattern,
+                ReceiptProperty.Operation => $"{receipt.Operation:D}" == pattern,
+                ReceiptProperty.TotalSum => $"{receipt.RoundedSum}" == pattern,
+                _ => false
+            };
+
+        private static bool CheckPosition(Position position, PositionProperty type, string pattern) =>
+            type switch
+            {
+                PositionProperty.Name => Regex.IsMatch(position.Name, pattern),
+                PositionProperty.Measure => $"{position.MeasureUnit:D}" == pattern,
+                PositionProperty.Payment => $"{position.PayType:D}" == pattern,
+                PositionProperty.Price => $"{position.Price}" == pattern,
+                PositionProperty.Quantity => $"{position.Quantity}" == pattern,
+                PositionProperty.Sum => $"{position.TotalSum}" == pattern,
+                PositionProperty.Tax => $"{position.TaxRate:D}" == pattern,
+                PositionProperty.Type => $"{position.PosType:D}" == pattern,
+                _ => false
+            };
 
         private static bool Check(Receipt receipt)
         {
@@ -122,21 +338,42 @@ namespace RetailCorrector.Wizard.Pages
         public Editor()
         {
             Instance = this;
-            Items = [];
+            FilterItems = [];
+            EditorItems = [];
             InitializeComponent();
         }
 
-        private void AddParam(object sender, RoutedEventArgs e)
+        private void AddParamFilter(object sender, RoutedEventArgs e)
         {
-            Items.Add(new FilterItem());
-            RefreshPreview();
+            FilterItems.Add(new FilterItem());
+            RefreshFilterPreview();
         }
 
-        private void RemoveParam(object sender, RoutedEventArgs e)
+        private void RemoveParamFilter(object sender, RoutedEventArgs e)
         {
             if (sender is not MenuItem i || i.Parent is not ContextMenu c || c.PlacementTarget is not DataGrid d || d.SelectedIndex == -1) return;
-            Items.RemoveAt(d.SelectedIndex);
-            RefreshPreview();
+            FilterItems.RemoveAt(d.SelectedIndex);
+            RefreshFilterPreview();
+        }
+
+        private void AddParamEditor(object sender, RoutedEventArgs e)
+        {
+            EditorItems.Add(new EditorItem());
+            RefreshEditorPreview();
+        }
+
+        private void RemoveParamEditor(object sender, RoutedEventArgs e)
+        {
+            if (sender is not MenuItem i || i.Parent is not ContextMenu c || c.PlacementTarget is not DataGrid d || d.SelectedIndex == -1) return;
+            EditorItems.RemoveAt(d.SelectedIndex);
+            RefreshEditorPreview();
+        }
+
+        private void SelectionTabChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.OriginalSource != sender || e.RemovedItems.Count == 0 || 
+                (string)((TabItem)e.AddedItems[0]!).Header != "Редактор") return;
+            RefreshEditorPreview();
         }
     }
 }
