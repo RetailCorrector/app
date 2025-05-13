b63c8345a1c124ccac8106bfd1324d8685818c98
ornaras <ornaras.us@gmail.com>
2025-05-13 15:14:37 +0400

    Добавлено отображение лога сборщика

--- Pages/Publisher.xaml.cs
+++ Pages/Publisher.xaml.cs
@@ -30,6 +30,43 @@ namespace RetailCorrector.Wizard.Pages
         }
         private RepoPackage _current;
 
+        public int MaxProgress 
+        {
+            get => _maxProgress;
+            set
+            {
+                _maxProgress = value;
+                OnPropertyChanged();
+                CurrProgress = 0;
+                LogText = "";
+            }
+        }
+        private int _maxProgress;
+
+        public string LogText
+        {
+            get => _log;
+            set
+            {
+                if (_log == value) return;
+                _log = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _log;
+
+        public int CurrProgress 
+        {
+            get => _currProgress;
+            set
+            {
+                if (_currProgress == value) return;
+                _currProgress = value; 
+                OnPropertyChanged();
+            }
+        }
+        private int _currProgress;
+
         public string TipText => string.Join("\n  ", CurrentPackage?.ConfigTip ?? []);
 
         public string FiscalConfig
@@ -69,7 +106,7 @@ namespace RetailCorrector.Wizard.Pages
         {
             try
             {
-                CreateTempFolder();
+                MaxProgress = 8;
                 UnzipAgent();
                 DownloadFiscal();
                 SaveReceipts();
@@ -98,19 +135,24 @@ namespace RetailCorrector.Wizard.Pages
 
         private void CreateTempFolder()
         {
+            LogText += ">>> Создание временной папки...\n";
             var path = Path.Combine(AppContext.BaseDirectory, "Temp");
             Directory.CreateDirectory(path);
+            CurrProgress++;
         }
         private void UnzipAgent()
         {
+            LogText += ">>> Распаковка заготовки агента...\n";
             var path = Path.Combine(AppContext.BaseDirectory, "agent.zip");
             var temp = Path.Combine(AppContext.BaseDirectory, "Temp");
             using var fs = File.OpenRead(path);
             using var zip = new ZipArchive(fs);
             zip.ExtractToDirectory(temp, true);
+            CurrProgress++;
         }
         private void DownloadFiscal()
         {
+            LogText += ">>> Скачивание фискальной интеграции...\n";
             using var http = new HttpClient();
             using var req = new HttpRequestMessage(HttpMethod.Get, _current.Url);
             using var resp = http.Send(req);
@@ -118,9 +160,11 @@ namespace RetailCorrector.Wizard.Pages
             var path = Path.Combine(AppContext.BaseDirectory, "Temp", "ExtFiscal.dll");
             using var fs = File.Create(path);
             stream.CopyTo(fs);
+            CurrProgress++;
         }
         private void SaveReceipts()
         {
+            LogText += ">>> Выгрузка отредактированых чеков...\n";
             var chunks = App.Receipts.Edited.Chunk(25).ToArray();
             for (var i = 0; i < chunks.Length; i++)
             {
@@ -129,38 +173,48 @@ namespace RetailCorrector.Wizard.Pages
                 Directory.CreateDirectory(Path.GetDirectoryName(path)!);
                 File.WriteAllText(path, text);
             }
+            CurrProgress++;
         }
         private void SaveReport()
         {
+            LogText += ">>> Выгрузка шаблона отчета...\n";
             var text = JsonSerializer.Serialize(App.Receipts.Report);
             var path = Path.Combine(AppContext.BaseDirectory, "Temp", "report.json");
             File.WriteAllText(path, text);
+            CurrProgress++;
         }
         private void Configure()
         {
+            LogText += ">>> Конфигурирование агента...\n";
             SetConfig("generic", "fiscal", FiscalConfig);
             SetConfig("generic", "persistence", $"{IsPersistence}");
+            CurrProgress++;
         }
         private void BuildInstaller()
         {
+            LogText += ">>> Упаковка агента...\n";
             var info = new ProcessStartInfo()
             {
                 FileName = Path.Combine(AppContext.BaseDirectory, "NSIS", "makensis.exe"),
                 Arguments = Path.Combine(AppContext.BaseDirectory, "Temp", "installer.nsi"),
             };
             Process.Start(info)!.WaitForExit();
+            LogText += ">>> Сохранение установщика агента\n";
             var save = new SaveFileDialog
             {
                 FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"RCAgent_Setup.exe"),
                 Filter = "Исполняемый файл|*.exe"
             };
             save.ShowDialog();
-            File.Move(Path.Combine(AppContext.BaseDirectory, "Temp", "RCAgent_Setup.exe"), save.FileName);
+            LogText += $">>> Установщик агента находится по пути: {save.FileName}\n";
+            CurrProgress++;
         }
         private void ClearTempFolder()
         {
+            LogText += ">>> Удаление временных файлов...\n";
             var path = Path.Combine(AppContext.BaseDirectory, "Temp");
             Directory.Delete(path, true);
+            CurrProgress++;
         }
 
         private static void SetConfig(string section, string key, string value)
