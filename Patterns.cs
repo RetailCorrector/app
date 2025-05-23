41d5159401cd936fa930fe1203c91b840440e7c1
ornaras <ornaras.us@gmail.com>
2025-05-23 12:09:57 +0400

    Добавлен шаблон вывода лог-записи

---
+++ Patterns.cs
@@ -0,0 +1,6 @@
+﻿namespace RetailCorrector.Constants;
+
+public static class Patterns
+{
+    public const string OutputLog = "{Timestamp:yyyy-MM-ss HH:mm:ss.ttt zzz} [{Level:u3}] ({Version}) {Message:lj}{NewLine}{Exception}";
+}
