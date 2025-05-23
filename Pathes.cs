f1e9c637d0f1b38cb8dbe9c75ff02eff38a826e3
ornaras <ornaras.us@gmail.com>
2025-05-23 12:45:24 +0400

    Добавлены пути для RegistryManager

--- Pathes.cs
+++ Pathes.cs
@@ -10,6 +10,7 @@ public static class Pathes
     #region Дочерние каталоги
     public readonly static string Wizard = Path.Combine(App, "Wizard");
     public readonly static string Cashier = Path.Combine(App, "Cashier");
+    public readonly static string RegistryManager = Path.Combine(App, "RegistryManager");
     public readonly static string Settings = Path.Combine(App, "Settings");
     public readonly static string Logs = Path.Combine(App, "Log");
     public readonly static string Modules = Path.Combine(App, "Modules");
@@ -20,5 +21,6 @@ public static class Pathes
     public readonly static string RegistryList = Path.Combine(Settings, "registry.list");
     public readonly static string WizardLog = Path.Combine(Logs, "wizard.log");
     public readonly static string CashierLog = Path.Combine(Logs, "cashier.log");
+    public readonly static string RegistryManagerLog = Path.Combine(Logs, "registry.log");
     #endregion
 }
