b4b657ce15b47aa5536175abd11fa5d43b792d24
ornaras <ornaras.us@gmail.com>
2025-05-23 12:09:20 +0400

    Добавлены пути для списка реестров, лог-файлов Wizard и Cashier

--- Pathes.cs
+++ Pathes.cs
@@ -1,22 +1,24 @@
-﻿namespace RetailCorrector.Constants
+﻿namespace RetailCorrector.Constants;
+
+public static class Pathes
 {
-    public static class Pathes
-    {
-        #region Родительные каталоги
-        public readonly static string App = 
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "RetailCorrector");
-        #endregion
+    #region Родительные каталоги
+    public readonly static string App = 
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "RetailCorrector"); 
+    #endregion
 
-        #region Дочерние каталоги
-        public readonly static string Wizard = Path.Combine(App, "Wizard");
-        public readonly static string Cashier = Path.Combine(App, "Cashier");
-        public readonly static string Settings = Path.Combine(App, "Settings");
-        public readonly static string Logs = Path.Combine(App, "Log");
-        public readonly static string Modules = Path.Combine(App, "Modules");
-        #endregion
+    #region Дочерние каталоги
+    public readonly static string Wizard = Path.Combine(App, "Wizard");
+    public readonly static string Cashier = Path.Combine(App, "Cashier");
+    public readonly static string Settings = Path.Combine(App, "Settings");
+    public readonly static string Logs = Path.Combine(App, "Log");
+    public readonly static string Modules = Path.Combine(App, "Modules");
+    #endregion
 
-        #region Файлы
-        public readonly static string Registry = Path.Combine(App, "registry.json");
-        #endregion
-    }
+    #region Файлы
+    public readonly static string IndexedRegistry = Path.Combine(App, "registry.json");
+    public readonly static string RegistryList = Path.Combine(Settings, "registry.list");
+    public readonly static string WizardLog = Path.Combine(Logs, "wizard.log");
+    public readonly static string CashierLog = Path.Combine(Logs, "cashier.log");
+    #endregion
 }
