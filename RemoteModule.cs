2fe5932bd59c5e11395a99b6f9ab9b6fd9955d98
ornaras <ornaras.us@gmail.com>
2025-05-26 13:57:40 +0400

    Добавлен JSON-конвертер для RemoteModule

--- RemoteModule.cs
+++ RemoteModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace RetailCorrector.RegistryManager;
 
@@ -8,17 +9,72 @@ public struct RemoteModules
     [JsonPropertyName("modules")] public RemoteModule[] Modules { get; set; }
 }
 
+[JsonConverter(typeof(RemoteModuleConverter))]
 public struct RemoteModule
 {
     [JsonConverter(typeof(JsonStringEnumConverter<ModuleType>))]
     public enum ModuleType { Fiscal, Source }
 
-    [JsonPropertyName("id")] public Guid Id { get; set; }
-    [JsonPropertyName("name")] public string Name { get; set; }
-    [JsonPropertyName("version")] public Version Version { get; set; }
-    [JsonPropertyName("type")] public ModuleType Type { get; set; }
-    [JsonPropertyName("author")] public string Author { get; set; }
-    [JsonPropertyName("description")] public string Description { get; set; }
-    [JsonPropertyName("file")] public string File { get; set; }
-    [JsonPropertyName("hash")] public string Hash { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Version Version { get; set; }
+    public ModuleType Type { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public string File { get; set; }
+    public string Hash { get; set; }
+}
+
+
+public class RemoteModuleConverter : JsonConverter<RemoteModule>
+{
+    public override RemoteModule Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+            throw new JsonException();
+        var module = new RemoteModule();
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject) break;
+            if (reader.TokenType != JsonTokenType.PropertyName)
+                throw new JsonException();
+            var propertyName = reader.GetString();
+            reader.Read();
+            switch (propertyName)
+            {
+                case "id":
+                    module.Id = reader.GetGuid();
+                    break;
+                case "name":
+                    module.Name = reader.GetString() ?? string.Empty;
+                    break;
+                case "version":
+                    module.Version = Version.Parse(reader.GetString() ?? "0.0.0");
+                    break;
+                case "type":
+                    module.Type = Enum.Parse<RemoteModule.ModuleType>(reader.GetString() ?? string.Empty, true);
+                    break;
+                case "author":
+                    module.Author = reader.GetString() ?? string.Empty;
+                    break;
+                case "description":
+                    module.Description = reader.GetString() ?? string.Empty;
+                    break;
+                case "file":
+                    module.File = reader.GetString() ?? string.Empty;
+                    break;
+                case "hash":
+                    module.Hash = reader.GetString() ?? string.Empty;
+                    break;
+                default:
+                    throw new JsonException($"Unexpected property: {propertyName}");
+            }
+        }
+        return module;
+    }
+
+    public override void Write(Utf8JsonWriter writer, RemoteModule value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
 }
