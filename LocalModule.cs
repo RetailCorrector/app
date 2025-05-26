3e5a133524e36b4eb773934beb6e36cdd97aa3e4
ornaras <ornaras.us@gmail.com>
2025-05-26 10:59:02 +0400

    Добавлена структура информации о локальном модулей

---
+++ LocalModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace RetailCorrector.RegistryManager
+{
+    public readonly struct LocalModule(Assembly assembly, byte[] data)
+    {
+        public Guid Id { get; } = new Guid(assembly.GetCustomAttribute<GuidAttribute>()!.Value);
+        public string Name { get; } = assembly.GetCustomAttribute<AssemblyTitleAttribute>()!.Title;
+        public Version Version { get; } = Version.Parse(assembly.GetCustomAttribute<AssemblyVersionAttribute>()!.Version);
+        public string Hash { get; } = CalculateSHA512(data);
+
+        private static string CalculateSHA512(byte[] data)
+        {
+            var hash = System.Security.Cryptography.SHA512.HashData(data);
+            var buffer = new byte[88];
+            System.Buffers.Text.Base64.EncodeToUtf8(hash, buffer, out _, out _);
+            return System.Text.Encoding.UTF8.GetString(buffer);
+        }
+    }
+}
