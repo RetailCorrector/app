7bea73859bdffbf919636bbde8f87920c153f834
ornaras <ornaras.us@gmail.com>
2025-05-26 14:00:20 +0400

    Добавлено объединение пути с созданием каталога

--- Pathes.cs
+++ Pathes.cs
@@ -7,34 +7,34 @@ public static class Pathes
     /// <code>C:\Program Files (x86)\RetailCorrector\</code>
     /// </summary>
     public readonly static string App = 
-        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "RetailCorrector");
+        SafeCombine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "RetailCorrector");
     #endregion
 
     #region Дочерние каталоги
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\Wizard\</code>
     /// </summary>
-    public readonly static string Wizard = Path.Combine(App, "Wizard");
+    public readonly static string Wizard = SafeCombine(App, "Wizard");
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\Cashier\</code>
     /// </summary>
-    public readonly static string Cashier = Path.Combine(App, "Cashier");
+    public readonly static string Cashier = SafeCombine(App, "Cashier");
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\RegistryManager\</code>
     /// </summary>
-    public readonly static string RegistryManager = Path.Combine(App, "RegistryManager");
+    public readonly static string RegistryManager = SafeCombine(App, "RegistryManager");
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\Settings\</code>
     /// </summary>
-    public readonly static string Settings = Path.Combine(App, "Settings");
+    public readonly static string Settings = SafeCombine(App, "Settings");
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\Logs\</code>
     /// </summary>
-    public readonly static string Logs = Path.Combine(App, "Logs");
+    public readonly static string Logs = SafeCombine(App, "Logs");
     /// <summary>
     /// <code>C:\Program Files (x86)\RetailCorrector\Modules\</code>
     /// </summary>
-    public readonly static string Modules = Path.Combine(App, "Modules");
+    public readonly static string Modules = SafeCombine(App, "Modules");
     #endregion
 
     #region Файлы
@@ -59,4 +59,16 @@ public static class Pathes
     /// </summary>
     public readonly static string RegistryManagerLog = Path.Combine(Logs, "registry.log");
     #endregion
+
+    private static string SafeCombine(params string[] paths)
+    {
+        var path = paths[0];
+        for(var i = 1; i < paths.Length; i++)
+        {
+            path = Path.Combine(path, paths[i]);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+        return path;
+    }
 }
