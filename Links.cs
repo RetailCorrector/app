5e5abc1a65f9366cfa3b8d565cbd2fed24913d97
ornaras <ornaras.us@gmail.com>
2025-05-26 11:11:34 +0400

    Добавлена ссылка на оффициальный реестр модулей

---
+++ Links.cs
@@ -0,0 +1,7 @@
+﻿namespace RetailCorrector.Constants
+{
+    public static class Links
+    {
+        public const string DefaultRegistry = "https://github.com/RetailCorrector/registry/raw/main/registry.json";
+    }
+}
