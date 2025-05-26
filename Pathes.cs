342ac7118ab191a6bcd722e793645ed58e72666e
ornaras <ornaras.us@gmail.com>
2025-05-26 10:10:59 +0400

    Исправление пути лог-директории

--- Pathes.cs
+++ Pathes.cs
@@ -12,7 +12,7 @@ public static class Pathes
     public readonly static string Cashier = Path.Combine(App, "Cashier");
     public readonly static string RegistryManager = Path.Combine(App, "RegistryManager");
     public readonly static string Settings = Path.Combine(App, "Settings");
-    public readonly static string Logs = Path.Combine(App, "Log");
+    public readonly static string Logs = Path.Combine(App, "Logs");
     public readonly static string Modules = Path.Combine(App, "Modules");
     #endregion
 
