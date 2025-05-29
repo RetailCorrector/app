27afc4e19b9224acafd2251ae168edc3b9a33e81
ornaras <ornaras.us@gmail.com>
2025-05-29 11:22:41 +0400

    Добавлены ключевые слова для шаблон-отчета

---
+++ ReportKeywords.cs
@@ -0,0 +1,15 @@
+﻿namespace RetailCorrector.Constants
+{
+    public static class ReportKeywords
+    {
+        public const string CurrentSession_Successes = "session";
+        public const string CurrentSession_Errors = "session:error";
+
+        public const string TotalCount = "total";
+
+        public const string LeftReceiptCount = "left";
+        public const string LeftMinutes = "left:minutes";
+        public const string LeftHours = "left:hours";
+        public const string LeftSessionCount = "left:sessions";
+    }
+}
