8b6211494d84ad1fd8d6e2937e123c3aa8411b10
ornaras <ornaras.us@gmail.com>
2025-05-29 12:13:01 +0400

    Перевод методов в команды

--- Windows/Main.xaml.cs
+++ Windows/Main.xaml.cs
@@ -1,14 +1,37 @@
 ﻿using RetailCorrector.Wizard.Contexts;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Input;
 
 namespace RetailCorrector.Wizard.Windows
 {
     public partial class Main : Window
     {
+        public RoutedCommand ShowReport { get; } = new RoutedCommand(nameof(ShowReport), typeof(Main));
+        public RoutedCommand ShowReceiptWizard { get; } = new RoutedCommand(nameof(ShowReceiptWizard), typeof(Main));
+        public RoutedCommand ShowParser { get; } = new RoutedCommand(nameof(ShowParser), typeof(Main));
+        public RoutedCommand ClearSpace { get; } = new RoutedCommand(nameof(ClearSpace), typeof(Main));
+
         public Main()
         {
+            SetupHotKeys();
             InitializeComponent();
+        }
+
+        private void SetupHotKeys()
+        {
+            ShowReport.InputGestures.Add(new KeyGesture(Key.R, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(ShowReport, (_, _) => new Report().ShowDialog()));
+            ShowParser.InputGestures.Add(new KeyGesture(Key.P, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(ShowParser, (_, _) => new Parser().ShowDialog()));
+            ShowReceiptWizard.InputGestures.Add(new KeyGesture(Key.P, ModifierKeys.Control | ModifierKeys.Alt));
+            CommandBindings.Add(new CommandBinding(ShowReceiptWizard, (_, _) => new ReceiptWizard().ShowDialog()));
+            ClearSpace.InputGestures.Add(new KeyGesture(Key.N, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(ClearSpace, (_, _) =>
+            {
+                WizardDataContext.Report = new RetailCorrector.Report();
+                WizardDataContext.Receipts.Clear();
+            }));
         }
 
         private void ShowLogs(object? s, RoutedEventArgs e) =>
@@ -19,20 +42,5 @@ namespace RetailCorrector.Wizard.Windows
 
         private void ShowAbout(object? s, RoutedEventArgs args) => 
             new About().ShowDialog();
-
-        private void ShowReport(object? s, RoutedEventArgs args) =>
-            new Report().ShowDialog();
-
-        private void ShowParser(object s, RoutedEventArgs args) =>
-            new Parser().ShowDialog();
-
-        private void ShowReceiptWizard(object s, RoutedEventArgs args) =>
-            new ReceiptWizard().ShowDialog();
-
-        private void ClearSpace(object s, RoutedEventArgs args)
-        {
-            WizardDataContext.Report = new RetailCorrector.Report();
-            WizardDataContext.Receipts.Clear();
-        }
     }
 }
