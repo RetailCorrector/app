99b546505717e9fc758e3f855f407b930f741b8b
ornaras <ornaras.us@gmail.com>
2025-05-29 11:22:02 +0400

    Добавлена ссылка на документацию

--- Links.cs
+++ Links.cs
@@ -2,7 +2,8 @@
 {
     public static class Links
     {
-        public const string DefaultRegistry = 
-            "https://raw.githubusercontent.com/RetailCorrector/registry/refs/heads/main/registry.json";        
+        public const string DefaultRegistry =
+            "https://raw.githubusercontent.com/RetailCorrector/registry/refs/heads/main/registry.json";
+        public const string Wiki = "https://retailcorrector.gitbook.io/wiki";
     }
 }
