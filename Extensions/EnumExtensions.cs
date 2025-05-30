3eae43e95a23a8ead3dba0fe644ac43eea869079
ornaras <ornaras.us@gmail.com>
2025-05-30 12:56:24 +0400

    Добавлена вариант метода GetDisplayNames с обобщенным аргументом

--- Extensions/EnumExtensions.cs
+++ Extensions/EnumExtensions.cs
@@ -5,6 +5,21 @@ namespace RetailCorrector.Wizard.Extensions
 {
     public static class EnumExtensions
     {
+        public static KeyValuePair<TEnum, string>[] GetDisplayNames<TEnum>()
+            where TEnum : struct, Enum
+        {
+            var values = Enum.GetValues<TEnum>();
+            var list = new List<KeyValuePair<TEnum, string>>();
+            for(var i = 0; i < values.Length; i++)
+            {
+                var value = values[i]!;
+                var memb = typeof(TEnum).GetMember(value.ToString()!)[0];
+                var attr = memb.GetCustomAttribute<DisplayAttribute>();
+                if (attr is null) continue;
+                list.Add(new KeyValuePair<TEnum, string>(value, attr!.Name!));
+            }
+            return [..list];
+        }
         public static Array GetDisplayNames(Type type)
         {
             var values = Enum.GetValues(type);
