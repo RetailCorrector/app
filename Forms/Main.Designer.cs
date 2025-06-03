c6987e7b038c10a82c0520248c0cd2aae914c844
ornaras <ornaras.us@gmail.com>
2025-06-03 12:07:09 +0400

    Реализация новой главной формы

---
+++ Forms/Main.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace RetailCorrector.Cashier.Forms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            fiscalModules = new ComboBox();
+            label1 = new Label();
+            statusStrip1 = new StatusStrip();
+            progress = new ToolStripProgressBar();
+            status = new ToolStripStatusLabel();
+            btnRefresh = new Button();
+            btnCancel = new Button();
+            btnStart = new Button();
+            panel1 = new Panel();
+            table = new TableLayoutPanel();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown1 = new NumericUpDown();
+            label2 = new Label();
+            statusStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // fiscalModules
+            // 
+            fiscalModules.FormattingEnabled = true;
+            fiscalModules.Location = new Point(139, 41);
+            fiscalModules.Name = "fiscalModules";
+            fiscalModules.Size = new Size(185, 23);
+            fiscalModules.TabIndex = 0;
+            fiscalModules.SelectionChangeCommitted += fiscalModules_SelectionChangeCommitted;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Фискальный модуль:";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { progress, status });
+            statusStrip1.Location = new Point(0, 466);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(367, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // progress
+            // 
+            progress.Name = "progress";
+            progress.Size = new Size(100, 16);
+            // 
+            // status
+            // 
+            status.Name = "status";
+            status.Size = new Size(0, 17);
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Font = new Font("Segoe MDL2 Assets", 10F);
+            btnRefresh.Location = new Point(330, 41);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(25, 23);
+            btnRefresh.TabIndex = 3;
+            btnRefresh.Text = "";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(199, 440);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 4;
+            btnCancel.Text = "Отмена";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(280, 440);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 5;
+            btnStart.Text = "Запустить";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.Controls.Add(table);
+            panel1.Location = new Point(12, 70);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(343, 367);
+            panel1.TabIndex = 6;
+            // 
+            // table
+            // 
+            table.AutoSize = true;
+            table.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            table.ColumnCount = 2;
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            table.Location = new Point(0, 0);
+            table.Name = "table";
+            table.RowCount = 1;
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            table.Size = new Size(0, 0);
+            table.TabIndex = 8;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(199, 12);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(75, 23);
+            numericUpDown2.TabIndex = 12;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(280, 12);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(75, 23);
+            numericUpDown1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(175, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Предельные размеры буфера:";
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(367, 488);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Controls.Add(btnStart);
+            Controls.Add(btnCancel);
+            Controls.Add(btnRefresh);
+            Controls.Add(statusStrip1);
+            Controls.Add(label1);
+            Controls.Add(fiscalModules);
+            HelpButton = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(383, 527);
+            MinimizeBox = false;
+            MinimumSize = new Size(383, 527);
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Локальный кассир";
+            HelpButtonClicked += OpenDocs;
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox fiscalModules;
+        private Label label1;
+        private StatusStrip statusStrip1;
+        private ToolStripProgressBar progress;
+        private ToolStripStatusLabel status;
+        private Button btnRefresh;
+        private Button btnCancel;
+        private Button btnStart;
+        private Panel panel1;
+        private TableLayoutPanel table;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
+    }
+}
---
+++ Forms/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace RetailCorrector.Cashier.Forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fiscalModules_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenDocs(object sender, CancelEventArgs e) =>
+            Process.Start(new ProcessStartInfo(Links.Wiki) { UseShellExecute = true });
+    }
+}
