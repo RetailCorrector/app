a4a0aee204cc79b0262a88465be0e26ad0dd7603
ornaras <ornaras.us@gmail.com>
2025-06-04 12:31:34 +0400

    Перевод AssemblyNameRegex в Constants.Patterns

--- Patterns.cs
+++ Patterns.cs
@@ -1,6 +1,11 @@
-﻿namespace RetailCorrector.Constants;
+﻿using System.Text.RegularExpressions;
 
-public static class Patterns
+namespace RetailCorrector.Constants;
+
+public static partial class Patterns
 {
     public const string OutputLog = "{Timestamp:yyyy-MM-ss HH:mm:ss.fff zzz} [{Level:u3}] ({Version}) {Message:lj}{NewLine}{Exception}";
+
+    [GeneratedRegex(@"^(?'name'[a-zA-Z _-]+) \((?'guid'[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12})\)$")]
+    public static partial Regex AssemblyNameRegex();
 }
