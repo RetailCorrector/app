1ac4e48c46d275727742d97572897cd81a9458c8
ornaras <ornaras.us@gmail.com>
2025-06-09 12:13:39 +0400

    Отделение PositionInGrid

--- RetailCorrector/Utils/UIExtensions.cs
+++ RetailCorrector/Utils/UIExtensions.cs
@@ -8,11 +8,16 @@ public static class UIExtensions
     public static void AddChild(this Grid parent, UIElement child, 
         int x = 0, int y = 0, int dx = 1, int dy = 1)
     {
+        PositionInGrid(child, x, y, dx, dy);
+        parent.Children.Add(child);
+    }
+    public static void PositionInGrid(UIElement child, 
+        int x = 0, int y = 0, int dx = 1, int dy = 1)
+    {
         if (x != 0) Grid.SetColumn(child, x);
         if (y != 0) Grid.SetRow(child, y);
         if (dx != 1) Grid.SetColumnSpan(child, dx);
         if (dy != 1) Grid.SetRowSpan(child, dy);
-        parent.Children.Add(child);
     }
 
     public static void AddColumn(this Grid parent, GridLength width) =>
