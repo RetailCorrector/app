4dafdf0a39ffb9c772436ef16100995b8acfa5ad
ornaras <ornaras.us@gmail.com>
2025-06-24 12:23:06 +0400

    hotfix: Убрано оповещение о записи в лог

--- RetailCorrector/History/HistoryController.cs
+++ RetailCorrector/History/HistoryController.cs
@@ -12,7 +12,7 @@ namespace RetailCorrector.History
         public static void Undo()
         {
             if (_currentIndex == -1) return;
-            Alert.Info($"Отменяется действие: {_current!.Action.DisplayName}...");
+            Alert.Debug($"Отменяется действие: {_current!.Action.DisplayName}...");
             _current.Action.Undo();
             var _previous = _current.Previous;
             _current = _previous;
@@ -25,7 +25,7 @@ namespace RetailCorrector.History
             if (_currentIndex == DisplayItems.Count - 1) return;
             var _next = (_current?.Next) ?? _begin;
             if (_next is null) return;
-            Alert.Info($"Выполняется действие: {_next.Action.DisplayName}...");
+            Alert.Debug($"Выполняется действие: {_next.Action.DisplayName}...");
             _next.Action.Redo();
             _current = _next;
             _currentIndex++;
@@ -34,7 +34,7 @@ namespace RetailCorrector.History
 
         public static void Add(IHistoryAction action)
         {
-            Alert.Info($"Выполняется действие: {action.DisplayName}...");
+            Alert.Debug($"Выполняется действие: {action.DisplayName}...");
             action.Redo();
             if (_currentIndex == -1)
                 _begin = _current = new Item(action);
@@ -52,7 +52,7 @@ namespace RetailCorrector.History
 
         public static void Clear()
         {
-            Alert.Info("Очистка истории...");
+            Alert.Debug("Очистка истории...");
             _currentIndex = -1;
             _current = _begin = null;
             DisplayItems.Clear();
