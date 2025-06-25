b5e895072f815dad4fdd14a550ed18152d386145
ornaras <ornaras.us@gmail.com>
2025-06-25 15:43:15 +0400

    Реализован SQL-редактор чеков

---
+++ RetailCorrector/Editor/EditorView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailCorrector.Storage;
+using RetailCorrector.Utils;
+using System.Data;
+using System.Windows.Controls;
+
+namespace RetailCorrector.Editor
+{
+    public partial class EditorView : UserControl
+    {
+        [NotifyUpdated] private string _queryText = "";
+        [NotifyUpdated] private DataTable _table = new();
+
+        public EditorView()
+        {
+            InitializeComponent();
+            StorageContext.Init();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var conn = StorageContext.Instance.Database.GetDbConnection();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = _queryText;
+            cmd.CommandType = CommandType.Text;
+            var table = new DataTable();
+            using var reader = cmd.ExecuteReader();
+            table.Load(reader);
+            Table = table;
+        }
+    }
+}
