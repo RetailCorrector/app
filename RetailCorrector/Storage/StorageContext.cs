eafa3ccfb2a0c95ccea4722d0bd8672922951d15
ornaras <ornaras.us@gmail.com>
2025-06-26 15:46:25 +0400

    Реализация песочницы

--- RetailCorrector/Storage/StorageContext.cs
+++ RetailCorrector/Storage/StorageContext.cs
@@ -7,6 +7,10 @@ namespace RetailCorrector.Storage
     public partial class StorageContext: DbContext
     {
         public DbSet<Models.Receipt> Receipts { get; set; }
+        private DbSet<Models.Position> Positions { get; set; }
+        private DbSet<Models.PositionCode> PositionCodes { get; set; }
+        private DbSet<Models.IndustryData> IndustryData { get; set; }
+
         public static StorageContext Instance { get; private set; } = null!;
         [NotifyUpdated] private bool _useSandbox = false;
 
@@ -25,7 +29,16 @@ namespace RetailCorrector.Storage
         {
             try
             {
-                var conn = Database.GetDbConnection();
+                SqliteConnection conn;
+                StorageContext ctx;
+                if (UseSandbox)
+                {
+                    ctx = new StorageContext();
+                    MigrateTo(ctx);
+                    conn = ctx.connection;
+                }
+                else
+                    conn = connection;
                 using var cmd = conn.CreateCommand();
                 cmd.CommandText = query;
                 cmd.CommandType = CommandType.Text;
@@ -39,6 +52,15 @@ namespace RetailCorrector.Storage
                 Alert.Error("При выполнении запроса возникла ошибка...", ex);
                 return null;
             }
+        }
+
+        private void MigrateTo(StorageContext other)
+        {
+            PositionCodes.ToList().ForEach(i => other.PositionCodes.Add(i));
+            IndustryData.ToList().ForEach(i => other.IndustryData.Add(i));
+            Positions.ToList().ForEach(i => other.Positions.Add(i));
+            Receipts.ToList().ForEach(i => other.Receipts.Add(i));
+            other.SaveChanges();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
