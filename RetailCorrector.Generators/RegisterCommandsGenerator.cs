6a1c57bdb624198725d6050b5fa1a6faa8740575
ornaras <ornaras.us@gmail.com>
2025-07-04 15:41:53 +0400

    Очистка RegisterCommandsGenerator

--- RetailCorrector.Generators/RegisterCommandsGenerator.cs
+++ RetailCorrector.Generators/RegisterCommandsGenerator.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace RetailCorrector.Generators
 {
     [Generator]
-    public class RegisterCommands : IIncrementalGenerator
+    public class RegisterCommandsGenerator : IIncrementalGenerator
     {
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
@@ -23,24 +23,24 @@ namespace RetailCorrector.Generators
                 return;
             var type = (ITypeSymbol)source;
             var members = type.GetMembers().Where(m => m is IFieldSymbol).ToArray();
-            builder.AppendLine(@$"using System.Windows.Input;
-
-namespace RetailCorrector.Utils;
-
-public static partial class Commands
-{{");
+            builder.AppendLine("""                
+                using System.Windows.Input;
+                
+                namespace RetailCorrector.Utils;
+                
+                public static partial class Commands
+                {{                
+                """);
             foreach (var prop in members)
                 builder.AppendLine($"    public static RoutedCommand {prop.Name} {{ get; }} = new(nameof({prop.Name}), typeof(Commands));");
-            builder.AppendLine(@"
-    private static void SetupHotKeys()
-    {");
+            builder.AppendLine("""                
+                    private static void SetupHotKeys()                
+                    {
+                """);
             foreach (var prop in members)
                 builder.AppendLine($"        {prop.Name}.InputGestures.Add(HotKeys.{prop.Name});");
             builder.AppendLine(@"    }
 }");
-
-            //public static RoutedCommand Undo { get; } = new(nameof(Undo), typeof(Commands));
-            //Undo.InputGestures.Add(HotKeys.Undo);
 
             ctx.AddSource("Commands.g.cs", builder.ToString());
         }
