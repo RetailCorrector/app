48628a1264bb3ed9024f85de1616a2d0b57d66cf
ornaras <ornaras.us@gmail.com>
2025-07-04 15:48:28 +0400

    hotfix: RegisterCommandsGenerator

--- RetailCorrector.Generators/RegisterCommandsGenerator.cs
+++ RetailCorrector.Generators/RegisterCommandsGenerator.cs
@@ -29,18 +29,21 @@ namespace RetailCorrector.Generators
                 namespace RetailCorrector.Utils;
                 
                 public static partial class Commands
-                {{                
+                {                
                 """);
             foreach (var prop in members)
                 builder.AppendLine($"    public static RoutedCommand {prop.Name} {{ get; }} = new(nameof({prop.Name}), typeof(Commands));");
-            builder.AppendLine("""                
+            builder.AppendLine("""
+                
                     private static void SetupHotKeys()                
                     {
                 """);
             foreach (var prop in members)
                 builder.AppendLine($"        {prop.Name}.InputGestures.Add(HotKeys.{prop.Name});");
-            builder.AppendLine(@"    }
-}");
+            builder.AppendLine("""
+                    }
+                }
+                """);
 
             ctx.AddSource("Commands.g.cs", builder.ToString());
         }
